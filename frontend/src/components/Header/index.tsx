59582b967a7895d4cad75fb3f7050522b3a18b4f
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-11-18 11:18:38 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import "./styles.css"
+import logo from "../../assets/img/logo.svg"
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Developed by
+                        <a href="https://www.linkedin.com/feed/"> Cairo Augusto de Andrade</a>
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
