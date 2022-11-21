34303f79637a98fd4d3d62272090a504df8f13f8
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-11-21 10:20:33 -0300

    Toast info message

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from '../../assets/img/notification-icon.svg';
 import './styles.css';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 
 type Props = {
     saleID: number;
@@ -10,7 +11,7 @@ type Props = {
 function hadleClick (id : number) {
     axios(`${BASE_URL}/sales/${id}/notification`).then(
         response => {
-            console.log("SUCESS");
+            toast.info("SMS sent successfully!");
         }
     );
 }
