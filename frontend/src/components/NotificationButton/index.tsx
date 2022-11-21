40ccbb7a6b26bbeb5f1b16d732839deba207b785
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2022-11-21 10:00:14 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,14 +1,20 @@
 import icon from '../../assets/img/notification-icon.svg';
 import './styles.css';
 import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
 
 type Props = {
     saleID: number;
 }
 
 function hadleClick (id : number) {
-    axios
+    axios(`${BASE_URL}/sales/${id}/notification`).then(
+        response => {
+            console.log("SUCESS");
+        }
+    );
 }
+
 function NotificationButton({saleID} : Props) {
     return (
         <>
